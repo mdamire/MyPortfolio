4268b3e335ac1e9980e4e4d370363c634c871a22
Amir Ebrahim <md.amire02@gmail.com>
2022-03-28 17:11:09 +0200

    added docker and circleci

--- basehome/tests.py
+++ basehome/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
+from django.urls import reverse
 
 # Create your tests here.
+
+class HomeTest(TestCase):
+
+    def test_home_return(self):
+        """To test if home page is working correctly
+        """
+
+        response = self.client.get(reverse('home'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'Amir Ebrahim')
