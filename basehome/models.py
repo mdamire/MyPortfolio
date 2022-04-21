aacace45729a9b67ed13381f11f8810327482972
Amir Ebrahim <md.amire02@gmail.com>
2022-04-21 18:34:28 +0200

    File upload created
    A new model has been created in basehome app to upload file.
    location of CV in about page has updated to take it from media directory.

--- basehome/models.py
+++ basehome/models.py
@@ -7,3 +7,10 @@ class Image(models.Model):
 
     def __str__(self):
         return self.title
+
+class Files(models.Model):
+    title = models.CharField(max_length=100)
+    file = models.FileField(upload_to='files/')
+
+    def __str__(self):
+        return self.title
