c692f4a23fd22a9fcd2eae8977cf11c847600bf1
mdamire <md.amire02@gmail.com>
2023-02-26 05:01:28 +0100

    Tags in sidebar title fixed

--- basehome/utils.py
+++ basehome/utils.py
@@ -4,12 +4,14 @@ def get_sidebar_section_links(body_text):
     """This will find all the tags in a posts body that has id='section-*'.
     """
 
-    regex = r'<.*?id="(section.*?)".*?>(.*?)<'
+    regex = r'<.*?id="(section.*?)".*?>(.+?)<'
     sections = []
     for match in re.finditer(regex, body_text):
+        title = '- ' + str(match[2]).title()
+        title = re.sub(r'<.*?>', '', title)
         sections.append(
             {
-                'label': str(match[2]).title(),
+                'label': title,
                 'url': '#' + str(match[1])
             }
         )
