c367986951f29f4e7446e6689a82396fd6bd5ab4
mdamire <md.amire02@gmail.com>
2024-11-29 11:17:32 +0100

    Back to top button added for post detail page

--- posts/static/posts/post-detail.js
+++ posts/static/posts/post-detail.js
@@ -17,3 +17,17 @@ if (collapseButton){
         addCollapseSign()
     });
 }
+
+// Add this JavaScript to your script file
+window.onscroll = function() {
+    var button = document.getElementById("back-to-top");
+    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+        button.style.display = "block";
+    } else {
+        button.style.display = "none";
+    }
+};
+
+function scrollToTop() {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+}
